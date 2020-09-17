1ee7c80a6f3c8d43eba3a7bbd853efafee86d34a
Marcelo11114 <dedosearte@hotmail.com>
2020-09-17 02:29:38 -0300

    Caixa Alta (gritador)

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string Frase;
+            Console.Clear();
+           Console.ForegroundColor = ConsoleColor.DarkBlue;
+           Console.WriteLine("!!!GRITADOR!!!");
+           Console.WriteLine(@"    ###");
+           Console.WriteLine(@"   |~ ~|");
+           Console.WriteLine(@"  (  -  )");
+           Console.WriteLine(@" //¨¨¨¨¨\\");
+           Console.ResetColor();
+
+
+           Console.WriteLine("\nDiga algo baixinho:");
+           Frase = Console.ReadLine().ToUpper();
+           Console.Clear();
+
+           Console.ForegroundColor = ConsoleColor.Yellow;
+           Console.WriteLine("!!!GRITADOR!!!");
+           Console.WriteLine(@"    ###");
+           Console.WriteLine(@"   |@ @|");
+           Console.WriteLine(@$" W(  O  )W    {Frase}");
+           Console.WriteLine(@" \\¨¨¨¨¨//");
+           Console.ResetColor();
+           //Console.WriteLine(Frase);
+                            
+
+        }
+    }
+}
